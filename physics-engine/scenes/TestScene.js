90b37e356332f1049091c03a06ed7f75ea4bd67d
Youssef Moawad <youssef_programming@icloud.com>
2019-05-22 15:02:11 +0100

    Introduced simple scene system and implemented a particle collection scene.

---
+++ physics-engine/scenes/TestScene.js
@@ -0,0 +1,54 @@
+let TestScene = function() {
+	this.collection = new EntityCollection();
+}
+
+TestScene.prototype.setup = function() {
+	createCanvas(windowWidth, windowHeight);
+	background(127);
+		
+	this.collection = new EntityCollection();
+	this.collection.setG(40);
+
+	let biggerEntity = new Entity(50, 100, 400);
+	let bigEntity = new Entity(100, 300, 400);
+	let smallEntity = new Entity(100, 600, 400);
+	let smallerEntity = new Entity(100, 1000, 400);
+
+	let rendererUpdate = function(e) {
+		e.renderer.update(e.position, e.radius(), 15+255*(e.density/0.1));
+	}
+
+	biggerEntity.setRestitution(0);
+	bigEntity.setRestitution(0);
+	smallEntity.setRestitution(0);
+	smallerEntity.setRestitution(0);
+
+	// bigEntity.setDensity(1);
+	// smallerEntity.setDensity(0.05);
+
+	biggerEntity.setRenderer(new CircleRenderer(), rendererUpdate);
+	bigEntity.setRenderer(new CircleRenderer(), rendererUpdate);
+	smallEntity.setRenderer(new CircleRenderer(), rendererUpdate);
+	smallerEntity.setRenderer(new CircleRenderer(), rendererUpdate);
+
+	// biggerEntity.applyImpulse(createVector(0, 100));
+	bigEntity.applyImpulse(createVector(0, -100));
+	// smallEntity.applyImpulse(createVector(0, 2000));
+	smallerEntity.applyImpulse(createVector(0, 170));
+	this.collection.addEntities([bigEntity, smallEntity, smallerEntity]);
+}
+
+TestScene.prototype.update = function() {
+	this.collection.update();
+}
+
+TestScene.prototype.render = function() {
+	background(127);
+
+	this.collection.render();
+
+	let fps = frameRate();
+	fill(255);
+	stroke(0);
+	text("FPS: " + fps.toFixed(2), 10, height - 10);
+}
--- physics-engine/script.js
+++ physics-engine/script.js
@@ -1,57 +1,10 @@
-let collection;
+let initialScene = new ParticleCollectionScene();
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(127);
-		
-	collection = new EntityCollection();
-	collection.setG(40);
-
-	let biggerEntity = new Entity(50, 100, 400);
-	let bigEntity = new Entity(50, 500, 400);
-	let smallEntity = new Entity(50, 900, 400);
-	let smallerEntity = new Entity(15, 1000, 400);
-
-	let rendererUpdate = function(e) {
-		e.renderer.update(e.position, e.radius(), 15+255*(e.density/0.1));
-	}
-
-	biggerEntity.setRestitution(0.01);
-	bigEntity.setRestitution(0.01);
-	smallEntity.setRestitution(0.01);
-
-	biggerEntity.setRenderer(new CircleRenderer(), rendererUpdate);
-	bigEntity.setRenderer(new CircleRenderer(), rendererUpdate);
-	smallEntity.setRenderer(new CircleRenderer(), rendererUpdate);
-	smallerEntity.setRenderer(new CircleRenderer(), rendererUpdate);
-
-	biggerEntity.applyImpulse(createVector(0, 50));
-	// bigEntity.applyImpulse(createVector(0, 100));
-	smallEntity.applyImpulse(createVector(0,-50));
-	// smallerEntity.applyImpulse(createVector(0, 50));
-	collection.addEntities([biggerEntity, bigEntity, smallEntity]);
-
-	// for(let i = 0; i < 10; i++) {
-	// 	let testEntity = new Entity(5, 0, 0);
-	// 	testEntity.setDragCoefficient(0.1);
-	// 	testEntity.applyImpulse(p5.Vector.random2D().mult(5));
-	
-	// 	collection.addEntity(testEntity);
-	// }
+	initialScene.setup();
 }
 
 function draw() {
-  background(127);
-
-	collection.update();
-	collection.render();
-
-	let fps = frameRate();
-	fill(255);
-	stroke(0);
-	text("FPS: " + fps.toFixed(2), 10, height - 10);
+	initialScene.update();
+	initialScene.render();
 }
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
